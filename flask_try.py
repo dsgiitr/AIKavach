d83f82387e801205b19060c61cdbf091eacad68e
Aryan Gupta <97878444+guptaaryan16@users.noreply.github.com>
2023-04-07 23:10:17 +0530

    "Add some backend features"

--- flask_try.py
+++ flask_try.py
@@ -3,8 +3,49 @@ import requests
 from flask import Flask, jsonify, request, Response, render_template
 from werkzeug.utils import secure_filename
 import requests
+import os
+import subprocess, sys, time
 
 app = Flask(__name__)
+
+# TODO:ADD the function and the parameters
+# I am imagining the command to be taking a file_name and some parameters and return the output file when it becomes available 
+'''
+The parameters to be taken by the file are 
+python dsrs/sampler.py mnist location_to_model_weights 0.50 --disttype general-gaussian --k 380 --N 50000 --alpha 0.0005 --skip 10 --batch 400
+
+1. Input_file_name
+2. Model(with layers)
+3. sigma: default=0.5
+4. Distributon_type: default=general-gaussian
+5. the training dataset : default=mnist
+6. k (or the formula to calculate k)
+7. N: default = 
+8. Alpha : default = 
+9. batch: default = 400
+'''
+def calculate_np_radius(
+  input_file_name, 
+  model,
+  sigma= 0.5,
+  Distributon_type="general-gaussian",
+  training = "mnist",
+  k =380,
+  N=100000,
+  Alpha = 0.05,
+  batch = 400
+):
+  # functions to mimic the subprocess
+  proc = subprocess.Popen(
+        ['bash', './nn.sh', 'localhost', '3'],
+        stdout=subprocess.PIPE,
+    )
+  outf = sys.stdout
+  for line in iter(proc.stdout.readline, ''):
+      outf.write( 'Reader thread ...\n' )
+      outf.write( line.rstrip() + '\n' )
+      outf.flush()
+
 
 # assume that "app" below is your flask app, and that
 # "Response" is imported from flask.
@@ -17,6 +58,7 @@ app = Flask(__name__)
 #     app.run(debug = True)
 
 @app.route('/upload')
+@app.route("/")
 def upload_file():
     return """
     <html>
@@ -36,8 +78,8 @@ def upload_file():
 def get_uploaded_file():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(secure_filename(f.filename))
-        
+        f.save(secure_filename(f.filename)) 
+        calculate_np_radius(f.filename, "resnet")
         return "file uploaded successfully"
     
 		
