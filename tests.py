f60c7c5f8d80d792ff24b6b74c74b25886022ca8
Aastha <aasthakhaitan1405@gmail.com>
2023-05-09 01:01:15 +0530

    Added tests for ISS

--- tests.py
+++ tests.py
@@ -14,7 +14,31 @@ class TestFinishedModel(unittest.TestCase):
         self.denoised = ChotaModel()
         self.model = FinishedModel(self.denoised, 784, 380, 10, 'general-gaussian', 'general-gaussian', 0.5, 0.4, 0.0005, num_sampling_min = 100)
         self.x = torch.randn((28, 28)).float()
-    
+
+    def num_samples(self):
+        num_sampling = 20000
+        fractional_loss = 0.02
+        batch_size = 64
+        nA1_1, realN_1_1 = smooth.get_logits(model = self.model.denoised_model, x = self.x, dist = self.model.dist_1, num = self.model.num_sampling_min, num_classes = self.model.num_classes, batch_size = batch_size, num_crop=1)
+        p1low_1, p1high_1 = smooth.confidence_bound(nA1_1[nA1_1.argmax().item()].item(), realN_1_1, self.model.alpha)
+        num_opt = self.model.get_opt_num_sampling(p1low_1, p1high_1, num_sampling, fractional_loss, batch_size, self.model.std_1, self.model.alpha)
+        self.assertGreater(num_opt,0) 
+
+    def test_iss_radius(self):
+        num_sampling = 20000
+        fractional_loss = 0.02
+        batch_size = 64
+        nA_1, realN_1 = smooth.get_logits(model = self.model.denoised_model, x = self.x, dist = self.model.dist_1, num = self.model.num_sampling_min, num_classes = self.model.num_classes, batch_size = batch_size)
+        p1low_1, p1high_1 = smooth.confidence_bound(nA_1[nA_1.argmax().item()].item(), realN_1, self.model.alpha)
+        r1,_ = self.model.orig_radius_pool_func(p1low_1, self.model.dist_1)
+        num_opt = self.model.get_opt_num_sampling(p1low_1, p1high_1, num_sampling, fractional_loss, batch_size, self.model.std_1, self.model.alpha)
+        nA_2, realN_2 = smooth.get_logits(model = self.model.denoised_model, x = self.x, dist = self.model.dist_1, num = num_opt*batch_size - self.model.num_sampling_min, num_classes = self.model.num_classes, batch_size = batch_size)
+        nA = nA_1 + nA_2
+        realN = realN_1 + realN_2
+        p1low_2, p1high_2 = smooth.confidence_bound(nA[nA.argmax().item()].item(), realN, self.model.alpha)
+        r2,_ = self.model.orig_radius_pool_func(p1low_2, self.model.dist_1)
+        self.assertGreater(r2,r1)
+        
     def test_bunk_radius(self):
         num_sampling = 20000
         batch_size = 64
