250f6f133929c800a0a5e60e2226ec7e520bdc4e
Devansh Bhardwaj <bhardwajdevansh398@gmail.com>
2023-05-27 00:40:39 +0530

    Changes in backend

--- train.py
+++ train.py
@@ -9,7 +9,8 @@ parser = argparse.ArgumentParser(description='Denoiser Training')
 # python3 train.py --epochs --dataset --input_dim --output_dim --model_name --model_path --no_of_channels --hidden_state_dim
 
 # Training Objective
-parser.add_argument('--epochs', type=int, help="Number of epochs")
+parser.add_argument('--epochs', type=int,
+                    help="Number of epochs")
 
 # Dataset
 parser.add_argument('--dataset', type=str, help='path to dataset of choice')
@@ -21,16 +22,17 @@ parser.add_argument('--model_name', type=str, help="name of model")
 parser.add_argument('--model_path', type=str, help="path to model")
 
 # Setting
+parser.add_argument('--nc', type=str, help='input should look like "265340,268738,270774,270817" ')
+parser.add_argument('--h', type=int, help='dimensions of hidden state')
 
 args = parser.parse_args()
 
-de=denoiser(in_nc = args.in_nc,out_nc=args.out_nc,nc = [64,128,256,512], nb=4)
+print(args.nc)
+
+a = [int(x) for x in args.nc.split()]
+
+de=denoiser(in_nc = args.in_nc,out_nc=args.out_nc,nc = a, nb=args.h)
 if args.model_path:
     de.ld(args.model_path)
 de.train_drunet(args.epochs,args.dataset)
 de.drunet.save(args.model_name)
-# <<<<<<< dev
-# de.drunet.save(args.model_name)
-# =======
-# de.drunet.save(args.model_name)
-# >>>>>>> main
